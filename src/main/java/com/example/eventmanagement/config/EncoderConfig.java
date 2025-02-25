2c10ac7de2e6573b9323080fde0af098a8f8e2b6
iqbalpa <iqbalpahlevi16@gmail.com>
2025-02-26 06:38:37 +0700

    chore: rename security config to encoder config

--- src/main/java/com/example/eventmanagement/config/EncoderConfig.java
+++ src/main/java/com/example/eventmanagement/config/EncoderConfig.java
@@ -1,6 +1,5 @@
 package com.example.eventmanagement.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.security.SecureRandom;
 
 @Configuration
-public class SecurityConfig {
+public class EncoderConfig {
     @Value("${encryption.salt}")
     private String salt;
 
